2ab9790b4987c83b6a071ccbb4e46b6659d644aa
abhivadya1 <abhivadya1@gmail.com>
2026-02-13 10:03:40 +0700

    testArithmeticAdd

--- src/test/java/com/example/individualprep/IndividualprepApplicationTests.java
+++ src/test/java/com/example/individualprep/IndividualprepApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.individualprep;
 
+import com.example.individualprep.service.ArithmeticUtility;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class IndividualprepApplicationTests {
-
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testArithmeticAdd() {
+		ArithmeticUtility arithmeticUtility = new ArithmeticUtility();
+		double result = arithmeticUtility.add(5.0, 3.0);
+		assertEquals(8.0, result, 0.0001);
 	}
 
 }
